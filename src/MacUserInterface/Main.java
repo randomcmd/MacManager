b4767b4751b2fcc7a3357c8f0f41cdaf05256583
randomcmd <mailrandomguys@gmail.com>
2020-06-04 09:19:45 +0200

    gui test

--- src/MacUserInterface/Main.java
+++ src/MacUserInterface/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     MacManager macManager;
 
     public static void main(String[] args) {
-        //launch(args);
+        launch(args);
         Main main = new Main();
         main.run();
     }
@@ -33,7 +33,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
         primaryStage.setTitle("Mac Manager");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 700, 500));
         primaryStage.show();
     }
 
